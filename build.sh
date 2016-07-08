08aec941eff674dc99edd945105a907755cba342
Mike Causer <mcauser@gmail.com>
2016-07-08 22:05:47 +1000

    Add Relay, Dual Base, Battery, Micro SD Shields

--- build.sh
+++ build.sh
@@ -1,45 +1,61 @@
 #!/bin/bash
 
-rm -rf dist/WeMos-D1-mini-1-Button-Shield-no-headers.fzpz
+rm -f dist/WeMos-D1-mini-1-Button-Shield-no-headers.fzpz
 zip -j dist/WeMos-D1-mini-1-Button-Shield-no-headers.fzpz src/part.WeMos-D1-mini-1-Button-Shield-no-headers.fzp
 zip -j dist/WeMos-D1-mini-1-Button-Shield-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-1-Button-Shield-no-headers_breadboard.svg
 zip -j dist/WeMos-D1-mini-1-Button-Shield-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-1-Button-Shield_pcb.svg
 zip -j dist/WeMos-D1-mini-1-Button-Shield-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-1-Button-Shield_schematic.svg
 
-rm -rf dist/WeMos-D1-mini-OLED-Shield-no-headers.fzpz
+rm -f dist/WeMos-D1-mini-Battery-Shield-no-headers.fzpz
+zip -j dist/WeMos-D1-mini-Battery-Shield-no-headers.fzpz src/part.WeMos-D1-mini-Battery-Shield-no-headers.fzp
+zip -j dist/WeMos-D1-mini-Battery-Shield-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-Battery-Shield-no-headers_breadboard.svg
+zip -j dist/WeMos-D1-mini-Battery-Shield-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-Battery-Shield_pcb.svg
+zip -j dist/WeMos-D1-mini-Battery-Shield-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-Battery-Shield_schematic.svg
+
+rm -f dist/WeMos-D1-mini-Dual-Base-Shield-no-headers.fzpz
+zip -j dist/WeMos-D1-mini-Dual-Base-Shield-no-headers.fzpz src/part.WeMos-D1-mini-Dual-Base-Shield-no-headers.fzp
+zip -j dist/WeMos-D1-mini-Dual-Base-Shield-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-Dual-Base-Shield-no-headers_breadboard.svg
+zip -j dist/WeMos-D1-mini-Dual-Base-Shield-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-Dual-Base-Shield_pcb.svg
+zip -j dist/WeMos-D1-mini-Dual-Base-Shield-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-Dual-Base-Shield_schematic.svg
+
+rm -f dist/WeMos-D1-mini-Micro-SD-Shield-no-headers.fzpz
+zip -j dist/WeMos-D1-mini-Micro-SD-Shield-no-headers.fzpz src/part.WeMos-D1-mini-Micro-SD-Shield-no-headers.fzp
+zip -j dist/WeMos-D1-mini-Micro-SD-Shield-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-Micro-SD-Shield-no-headers_breadboard.svg
+zip -j dist/WeMos-D1-mini-Micro-SD-Shield-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-Micro-SD-Shield_pcb.svg
+zip -j dist/WeMos-D1-mini-Micro-SD-Shield-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-Micro-SD-Shield_schematic.svg
+
+rm -f dist/WeMos-D1-mini-OLED-Shield-no-headers.fzpz
 zip -j dist/WeMos-D1-mini-OLED-Shield-no-headers.fzpz src/part.WeMos-D1-mini-OLED-Shield-no-headers.fzp
 zip -j dist/WeMos-D1-mini-OLED-Shield-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-OLED-Shield-no-headers_breadboard.svg
 zip -j dist/WeMos-D1-mini-OLED-Shield-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-OLED-Shield_pcb.svg
 zip -j dist/WeMos-D1-mini-OLED-Shield-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-OLED-Shield_schematic.svg
 
-rm -rf dist/WeMos-D1-mini-Protoboard-Shield-no-headers.fzpz
+rm -f dist/WeMos-D1-mini-Protoboard-Shield-no-headers.fzpz
 zip -j dist/WeMos-D1-mini-Protoboard-Shield-no-headers.fzpz src/part.WeMos-D1-mini-Protoboard-Shield-no-headers.fzp
 zip -j dist/WeMos-D1-mini-Protoboard-Shield-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-Protoboard-Shield-no-headers_breadboard.svg
 zip -j dist/WeMos-D1-mini-Protoboard-Shield-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-Protoboard-Shield_pcb.svg
 zip -j dist/WeMos-D1-mini-Protoboard-Shield-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-Protoboard-Shield_schematic.svg
 
-rm -rf dist/WeMos-D1-mini-WS2812B-RGB-Shield-no-headers.fzpz
+rm -f dist/WeMos-D1-mini-Relay-Shield-no-headers.fzpz
+zip -j dist/WeMos-D1-mini-Relay-Shield-no-headers.fzpz src/part.WeMos-D1-mini-Relay-Shield-no-headers.fzp
+zip -j dist/WeMos-D1-mini-Relay-Shield-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-Relay-Shield-no-headers_breadboard.svg
+zip -j dist/WeMos-D1-mini-Relay-Shield-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-Relay-Shield_pcb.svg
+zip -j dist/WeMos-D1-mini-Relay-Shield-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-Relay-Shield_schematic.svg
+
+rm -f dist/WeMos-D1-mini-Relay-Shield-V2-no-headers.fzpz
+zip -j dist/WeMos-D1-mini-Relay-Shield-V2-no-headers.fzpz src/part.WeMos-D1-mini-Relay-Shield-V2-no-headers.fzp
+zip -j dist/WeMos-D1-mini-Relay-Shield-V2-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-Relay-Shield-V2-no-headers_breadboard.svg
+zip -j dist/WeMos-D1-mini-Relay-Shield-V2-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-Relay-Shield-V2_pcb.svg
+zip -j dist/WeMos-D1-mini-Relay-Shield-V2-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-Relay-Shield-V2_schematic.svg
+
+rm -f dist/WeMos-D1-mini-WS2812B-RGB-Shield-no-headers.fzpz
 zip -j dist/WeMos-D1-mini-WS2812B-RGB-Shield-no-headers.fzpz src/part.WeMos-D1-mini-WS2812B-RGB-Shield-no-headers.fzp
 zip -j dist/WeMos-D1-mini-WS2812B-RGB-Shield-no-headers.fzpz src/svg.breadboard.WeMos-D1-mini-WS2812B-RGB-Shield-no-headers_breadboard.svg
 zip -j dist/WeMos-D1-mini-WS2812B-RGB-Shield-no-headers.fzpz src/svg.pcb.WeMos-D1-mini-WS2812B-RGB-Shield_pcb.svg
 zip -j dist/WeMos-D1-mini-WS2812B-RGB-Shield-no-headers.fzpz src/svg.schematic.WeMos-D1-mini-WS2812B-RGB-Shield_schematic.svg
 
 # demo
-rm -rf demo/Demo\ Sketch.fzz
-zip -j demo/Demo\ Sketch.fzz src/Demo\ Sketch.fz
-zip -j demo/Demo\ Sketch.fzz src/part.WeMos-D1-mini-1-Button-Shield-no-headers.fzp
-zip -j demo/Demo\ Sketch.fzz src/part.WeMos-D1-mini-OLED-Shield-no-headers.fzp
-zip -j demo/Demo\ Sketch.fzz src/part.WeMos-D1-mini-Protoboard-Shield-no-headers.fzp
-zip -j demo/Demo\ Sketch.fzz src/part.WeMos-D1-mini-WS2812B-RGB-Shield-no-headers.fzp
-zip -j demo/Demo\ Sketch.fzz src/svg.breadboard.WeMos-D1-mini-1-Button-Shield-no-headers_breadboard.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.breadboard.WeMos-D1-mini-OLED-Shield-no-headers_breadboard.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.breadboard.WeMos-D1-mini-Protoboard-Shield-no-headers_breadboard.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.breadboard.WeMos-D1-mini-WS2812B-RGB-Shield-no-headers_breadboard.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.pcb.WeMos-D1-mini-1-Button-Shield_pcb.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.pcb.WeMos-D1-mini-OLED-Shield_pcb.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.pcb.WeMos-D1-mini-Protoboard-Shield_pcb.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.pcb.WeMos-D1-mini-WS2812B-RGB-Shield_pcb.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.schematic.WeMos-D1-mini-1-Button-Shield_schematic.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.schematic.WeMos-D1-mini-OLED-Shield_schematic.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.schematic.WeMos-D1-mini-Protoboard-Shield_schematic.svg
-zip -j demo/Demo\ Sketch.fzz src/svg.schematic.WeMos-D1-mini-WS2812B-RGB-Shield_schematic.svg
+rm -f demo/Demo\ Sketch.fzz
+zip -j demo/Demo\ Sketch.fzz src/*.fz
+zip -j demo/Demo\ Sketch.fzz src/*.fzp
+zip -j demo/Demo\ Sketch.fzz src/*.svg
